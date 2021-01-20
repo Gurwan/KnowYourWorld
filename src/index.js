72e37dc1d60ec7bb39acd6540677e33b03bb8342
Gurwan Delaunay <gurwandelaunay35@gmail.com>
2021-01-20 08:26:14 +0100

    input fonctionnel : peut ajouter un pays à la liste

--- src/index.js
+++ src/index.js
@@ -2,19 +2,46 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+
+  input = React.createRef();
+
+  state = {
+    paysFound : [],
+    newPays: '',
+    compteurAfrique: 0,
+    compteurAmerique: 0,
+    compteurAsie: 0,
+    compteurEurope: 0,
+    compteurOceanie: 0
+  }
+
+  hChange = (event) => {
+    const v = event.currentTarget.value;
+    this.setState({newPays: v});
+  }
+
+  addCountry = (event) => {
+    event.preventDefault();
+    const countries = this.state.paysFound.slice();
+    countries.push(this.state.newPays);
+    this.setState({paysFound : countries});
+  }
+
   render(){
+
     return(
       <div>
         <h1>Countries Game</h1>
-        <form>
-          <input type="text" placeholder="Entrer un pays"/>
+        <form onSubmit={this.addCountry}>
+          <input value={this.state.newPays} onChange={this.hChange} type="text" placeholder="Entrer un pays"/>
           <button>Confirmer</button>
         </form>
-        <p>Pays en Afrique : 0/54</p>
-        <p>Pays en Amérique : 0/36</p>
-        <p>Pays en Asie : 0/47</p>
-        <p>Pays en Europe : 0/46</p>
-        <p>Pays en Océanie : 0/17</p>
+        <p>Pays en Afrique : {this.state.compteurAfrique}/54</p>
+        <p>Pays en Amérique : {this.state.compteurAmerique}/36</p>
+        <p>Pays en Asie : {this.state.compteurAsie}/47</p>
+        <p>Pays en Europe : {this.state.compteurEurope}/46</p>
+        <p>Pays en Océanie : {this.state.compteurOceanie}/17</p>
+        <ul>{this.state.paysFound.map(m => (<li>{m}</li>))}</ul>
       </div>
     );
   }
