f2b4046a7f1e30128a79f99128ee6027f7a9ed99
Gurwan Delaunay <gurwandelaunay35@gmail.com>
2021-01-20 12:38:50 +0100

    ajout des derniers pays

--- src/index.js
+++ src/index.js
@@ -7,16 +7,17 @@ class App extends React.Component {
     paysFound : [],
     paysAfrique : ["Afrique du Sud","Algérie","Angola","Bénin","Botswana","Burkina Faso","Burundi","Cameroun","Cap-Vert","République centrafricaine","Comores","République du Congo","République démocratique du Congo","Côte d'Ivoire","Djibouti","Égypte","Érythrée","Éthiopie","Eswatini","Gabon","Gambie","Ghana","Guinée","Guinée-Bissau","Guinée équatoriale","Kenya","Lesotho","Liberia","Libye","Madagascar","Malawi","Mali","Maroc","Maurice","Mauritanie","Mozambique","Namibie","Niger","Nigeria","Ouganda","Rwanda","Sao Tomé-et-Principe","Sénégal","Seychelles","Sierra Leone","Somalie","Soudan","Soudan du Sud","Tanzanie","Tchad","Togo","Tunisie","Zambie","Zimbabwe"],
     paysAmerique : ["Antigua-et-Barbuda","Argentine","Bahamas","Barbade","Belize","Bolivie","Brésil","Canada","Chili","Colombie","Costa Rica","Cuba","République dominicaine","Dominique","Équateur","États-Unis","Grenade","Guatemala","Guyana","Haïti","Honduras","Jamaïque","Mexique","Nicaragua","Panama","Paraguay","Pérou","Porto Rico","Saint-Christophe-et-Niévès","Sainte-Lucie","Saint-Vincent-et-les-Grenadines","Salvador","Suriname","Trinité-et-Tobago","Uruguay","Venezuela"],
-    paysAsie : [],
-    paysEuropeen: ["France","Espagne"],
+    paysAsie : ["Afghanistan","Arabie saoudite","Arménie","Azerbaïdjan","Bahreïn","Bangladesh","Bhoutan","Birmanie","Brunei","Cambodge","Chine","Corée du Nord","Corée du Sud","Émirats arabes unis","Géorgie","Inde","Indonésie","Irak","Iran","Israël","Japon","Jordanie","Kazakhstan","Kirghizistan","Koweït","Laos","Liban","Malaisie","Maldives","Mongolie","Népal","Oman","Ouzbékistan","Pakistan","Palestine","Philippines","Qatar","Russie","Singapour","Sri Lanka","Syrie","Taïwan","Tadjikistan","Thaïlande","Timor oriental","Turkménistan","Turquie","Viêtnam","Yémen"],
+    paysEuropeen: ["Albanie","Allemagne","Andorre","Autriche","Belgique","Biélorussie","Bosnie-Herzégovine","Bulgarie","Chypre","Croatie","Danemark","Espagne","Estonie","Finlande","France","Grèce","Hongrie","Irlande","Islande","Italie","Kosovo","Lettonie","Liechtenstein","Lituanie","Luxembourg","Macédoine du Nord","Malte","Moldavie","Monaco","Monténégro","Norvège","Pays-Bas","Pologne","Portugal","République tchèque","Roumanie","Royaume-Uni","Russie","Saint-Marin","Serbie","Slovaquie","Slovénie","Suède","Suisse","Ukraine","Vatican"],
     paysOceanie : ["Australie","Fidji","Kiribati","Îles Cook","Îles Mariannes du Nord","Îles Marshall","États fédérés de Micronésie","Nauru","Niue","Nouvelle-Zélande","Palaos","Papouasie-Nouvelle-Guinée","Salomon","Samoa","Tonga","Tuvalu","Vanuatu"],
     newPays: '',
     compteurAfrique: 0,
     compteurAmerique: 0,
     compteurAsie: 0,
     compteurEurope: 0,
-    compteurOceanie: 0
-  }
+    compteurOceanie: 0,
+    compteurTout: 0
+    }
 
   hChange = (event) => {
     const v = event.currentTarget.value;
@@ -31,6 +32,7 @@ class App extends React.Component {
     var cAm = this.state.compteurAmerique;
     var cAs = this.state.compteurAsie;
     var cO = this.state.compteurOceanie;
+    var cT = this.state.compteurTout;
     var p = this.state.newPays.toLowerCase();
     p = p.charAt(0).toUpperCase()+p.slice(1);
     p = p.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
@@ -92,18 +94,23 @@ class App extends React.Component {
     if(pE.includes(p) && !(this.state.paysFound.includes(this.state.paysEuropeen[pE.indexOf(p)]))){
       countries.push(this.state.paysEuropeen[pE.indexOf(p)]);
       this.setState({compteurEurope : cE+1});
+      this.setState({compteurTout : cT+1});
     } else if(pAf.includes(p) && !(this.state.paysFound.includes(this.state.paysAfrique[pAf.indexOf(p)]))){
       countries.push(this.state.paysAfrique[pAf.indexOf(p)]);
       this.setState({compteurAfrique : cAf+1});
+      this.setState({compteurTout : cT+1});
     } else if(pAm.includes(p) && !(this.state.paysFound.includes(this.state.paysAmerique[pAm.indexOf(p)]))){
       countries.push(this.state.paysAmerique[pAm.indexOf(p)]);
       this.setState({compteurAmerique : cAm+1});
+      this.setState({compteurTout : cT+1});
     } else if(pAs.includes(p) && !(this.state.paysFound.includes(this.state.paysAsie[pAs.indexOf(p)]))){
       countries.push(this.state.paysAsie[pAs.indexOf(p)]);
       this.setState({compteurAsie : cAs+1});
+      this.setState({compteurTout : cT+1});
     } else if(pO.includes(p) && !(this.state.paysFound.includes(this.state.paysOceanie[pO.indexOf(p)]))){
       countries.push(this.state.paysOceanie[pO.indexOf(p)]);
       this.setState({compteurOceanie : cO+1});
+      this.setState({compteurTout : cT+1});
     }
     this.setState({paysFound : countries});
   }
@@ -121,6 +128,7 @@ class App extends React.Component {
         <p>Pays en Asie : {this.state.compteurAsie}/{this.state.paysAsie.length}</p>
         <p>Pays en Europe : {this.state.compteurEurope}/{this.state.paysEuropeen.length}</p>
         <p>Pays en Océanie : {this.state.compteurOceanie}/{this.state.paysOceanie.length}</p>
+        <p>Total : {this.state.compteurTout}/{this.state.paysAfrique.length+this.state.paysAmerique.length+this.state.paysAsie.length+this.state.paysEuropeen.length+this.state.paysOceanie.length}</p>
         <ul>{this.state.paysFound.map(m => (<li>{m}</li>))}</ul>
       </div>
     );
